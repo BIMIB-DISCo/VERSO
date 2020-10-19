058e3bf99dea0846dbde3435ef5ebdc562f8a5d2
alexgraudenzi <alex.graudenzi@unimib.it>
2020-10-19 14:51:31 +0200

    update VERSO STEP #2
    update VERSO STEP #2

--- VERSO_STEP_2/VERSO_STEP_2.py
+++ VERSO_STEP_2/VERSO_STEP_2.py
@@ -1,7 +1,5 @@
-# No actions are required at this step, please just run the notebook 
-#from IPython.core.interactiveshell import InteractiveShell
-#InteractiveShell.ast_node_interactivity = "all"
-
+#VERSO_STEP_2.py
+#Script to run VERSO STEP #2. Please refer to the readme at this link: https://github.com/BIMIB-DISCo/VERSO/tree/VERSO/VERSO_STEP_2
 import os
 os.chdir(os.getcwd())
 os.getcwd()
@@ -42,20 +40,8 @@ def colormap(fig,nax):
     fig.delaxes(fig.get_axes()[-2])
 
 
- # Please LOAD the INPUT FILES with the following specifications: 
-
-# A) Variant Frequency (VF) Matrix, with n rows (variants) and m columns (samples), each entry includes the VF 
-#    for that variant in that sample. Pre-processing step might be required to impute NA values (e.g., due to
-#    low coverage). In the example analyses, NA's are imputed with 0's. 
-#    file format -> comma separated value (csv) file: input_data_matrix_filename = "xxx.csv"
-
-# B) Samples info matrix, with m rows (samples) and k columns (attributes), each entry includes the value of a 
-#    specific attribute, e.g., country, date, clade, etc., for any given sample. 
-#    file format -> comma separated value (csv) file: samples_info_matrix_filename = "yyy.csv"
-
-# C) Variants info matrix, with n rows (variants) and z columns (attributes), each entry includes the value of a 
-#    specific attribute for any variant to be considered in the analysis. 
-#    file format -> comma separated value (csv) file: variants_info_matrix_filename = "zzz.csv"
+# Please LOAD the INPUT FILES. For the specifications refer to the readme at this link: 
+#https://github.com/BIMIB-DISCo/VERSO/tree/VERSO/VERSO_STEP_2
 
 #(A)
 input_data_matrix_filename = "VF_matrix.csv"
@@ -66,7 +52,22 @@ samples_info_matrix_filename = "samples_info_matrix.txt"
 #(C)
 variants_info_matrix_filename = "variants_info_matrix.txt"
 
+#(D)
+options_file = "configuration_VERSO.txt"
 
+options = pd.read_csv(options_file, header=0, sep='\t')
+options.set_index("Parameter", drop=True, inplace=True)
+
+input_sample_filter = str(options[options.index=='sample_filter']['Value'][0])
+input_variant_filter = str(options[options.index=='variant_filter']['Value'][0])
+input_knn = int(options[options.index=='knn']['Value'])
+input_n_pca = int(options[options.index=='n_pca']['Value'])
+input_distance_metric = str(options[options.index=='distance_metric']['Value'][0])
+input_leiden_res = float(options[options.index=='leiden_res']['Value'])
+input_a_UMAP = float(options[options.index=='a_UMAP']['Value'])
+input_b_UMAP = float(options[options.index=='b_UMAP']['Value'])
+input_spread_UMAP = float(options[options.index=='spread_UMAP']['Value'])
+input_min_dist_UMAP = float(options[options.index=='min_dist_UMAP']['Value'])
 
 #defining the data structures
 data_all = pd.read_csv(input_data_matrix_filename, header=0, sep=',')
@@ -81,84 +82,81 @@ adata_all=an.AnnData(data_all.T)
 adata_all.var_names_make_unique()
 adata_all.obs_names_make_unique()
 
-adata_all.obs['Genotype'] = 'NA'
-adata_all.obs['Outlier'] = 'NA'
-adata_all.obs['Contact_group'] = 'NA'
-adata_all.obs['Week'] = 'NA'
+for i in range(len(assignments.columns)):
+    temp = assignments.columns[i]
+    adata_all.obs[temp] = 'NA'
+    adata_all.obs[temp][:] = assignments[temp]
 
+#selecting the variants
+if input_variant_filter == 'yes':
+    list_selected_variants = metadata[metadata['Selected']==1].index
+    adata_all_subset_variants = adata_all[:, list_selected_variants]
+if input_variant_filter != 'yes':
+    adata_all_subset_variants = adata_all
 
-adata_all.obs['Genotype'][:] = assignments['Genotype']
-adata_all.obs['Outlier'][:] = assignments['outlier']
-adata_all.obs['Contact_group'][:] = assignments['Epi']
-adata_all.obs['Week'][:] = assignments['week']
-
-adata_all
-
-
-#selecting only the variants present in the dataset and those observed in at most 1 clonal genotype, 
-#excluding the clonal variants employed in the reconstruction of the phylogenetic tree via VERSO STEP #1
-list_variants_no_homoplasy = metadata[metadata['nCluster']<2].index
-list_variants_no_clonal = metadata[metadata['inference']==0].index
-index_final_variants = list_variants_no_homoplasy.intersection(list_variants_no_clonal)
-
-adata_all_subset_variants = adata_all[:, index_final_variants]
-adata_all_subset_variants
-
-#selecting only the samples that are NOT outliers (with <100 minor variants)
-adata_all_subset_variants_samples = adata_all_subset_variants[adata_all_subset_variants.obs['Outlier']==0].copy()
-adata_all_subset_variants_samples
-
+#selecting the samples
+if input_sample_filter == 'yes':
+    adata_all_subset_variants_samples = adata_all_subset_variants[adata_all_subset_variants.obs['Selected']==1].copy()
+if input_sample_filter != 'yes':
+    adata_all_subset_variants_samples = adata_all_subset_variants
 
 #performing VERSO STEP #2 on all clonal genotypes
-how_many_clusters = len(np.unique(adata_all_subset_variants_samples.obs['Genotype']))
-how_many_clusters
-for x in range(how_many_clusters):
+genotype_list = (np.unique(adata_all_subset_variants_samples.obs['Genotype']))
+how_many_clusters = len(genotype_list)
 
-    y = x + 1
-    if y<10:
-        print('C'+ str(0)+str(y))
-        temp = ('C'+ str(0)+str(y))
-        temp_title = ('Dataset #1 - Genotype '+ str(0)+str(y))
-    else: 
-        print('C'+str(y))
-        temp = ('C'+str(y))
-        temp_title = ('Dataset #1 - Genotype '+str(y))
+for x in range(how_many_clusters):
+    print('G'+str(genotype_list[x]))
+    temp = ('G'+str(genotype_list[x]))
+    temp_title = ('Genotype '+str(genotype_list[x]))
         
-    adata_all_cluster = adata_all_subset_variants_samples[adata_all_subset_variants_samples.obs['Genotype'] == temp].copy()
-    
+    adata_all_cluster = adata_all_subset_variants_samples[adata_all_subset_variants_samples.obs['Genotype'] == genotype_list[x]].copy()   
     how_many_samples = len(adata_all_cluster.obs)
-    temp_title = temp_title + ' (n = '+ str(how_many_samples) +')'
-    
     how_many_variables = len(adata_all_cluster.var)
-    
+
+    temp_title = temp_title + ' (n = '+ str(how_many_samples) +')'
+       
     if how_many_samples > 20:
-        number_of_neighbours_to_be_used = 10
-        number_of_PCA = 10
         
         sc.pp.pca(adata_all_cluster, n_comps=10,zero_center = 'False',random_state=1)       
-        sc.pp.neighbors(adata_all_cluster, knn = 'TRUE', n_pcs = number_of_PCA, 
-                        n_neighbors=number_of_neighbours_to_be_used,random_state=1,
-                        metric = 'braycurtis', method = "umap")
-        sc.tl.leiden(adata_all_cluster, key_added='groups', random_state=1,resolution = 1)
+        sc.pp.neighbors(adata_all_cluster, knn = 'TRUE', n_pcs = input_n_pca, 
+                        n_neighbors=input_knn,random_state=1,
+                        metric = input_distance_metric, method = "umap")
+        sc.tl.leiden(adata_all_cluster, key_added='Leiden_clusters', random_state=1,resolution = input_leiden_res)
 
-        sc.tl.umap(adata_all_cluster, random_state=1, a = 0.01, b = 1, spread = 1, min_dist = 2)
+        sc.tl.umap(adata_all_cluster, random_state=1, a = input_a_UMAP, b = input_b_UMAP, 
+                   spread = input_spread_UMAP, min_dist = input_min_dist_UMAP)
 
         temp_file = temp+'.svg'
         temp_output = 'output'+temp+'.csv'
         temp_file_distances = temp+'_distances.txt'
 
         if how_many_samples > 500:
-            size_nodes = 40
+            size_nodes = 50
         if how_many_samples <= 500:
             size_nodes = 100
-        sc.pl.umap(adata_all_cluster, color=['groups'], legend_loc='right margin', edges = 'TRUE', edges_width = 0.2, size = size_nodes, title = temp_title , show=False)       
-        #to visualize the contact information on the UMAP plots, uncomment the following line
-        # sc.pl.umap(adata_all_cluster, color=['Contact_group'], legend_loc='right margin', edges = 'TRUE', edges_width = 0.2, size = 50,  title = temp ,show=False)
+            
+            
+        for i in range(len(assignments.columns)):
+            if assignments.columns[i] != 'Genotype':
+            	if assignments.columns[i] != 'Selected':
+		            temp_file = temp+assignments.columns[i]+'.svg'
+		            temp_title_fin = temp_title + ' ' + assignments.columns[i]
+		            sc.pl.umap(adata_all_cluster, color=([str(assignments.columns[i])]), 
+		                       legend_loc='right margin', edges = 'TRUE', 
+		                       edges_width = 0.2, size = size_nodes,  
+		                       title = temp_title_fin , show=False)
+		            plt.savefig(temp_file,bbox_inches='tight')
         
-        plt.savefig(temp_file,bbox_inches='tight')
+        temp_file = temp + 'Leiden_clusters.svg'
+        temp_title_fin = temp_title + ' Leiden_clusters'
+        sc.pl.umap(adata_all_cluster, color=('Leiden_clusters'),                    
+                   legend_loc='right margin', edges = 'TRUE', 
+                   edges_width = 0.2, size = size_nodes,                     
+                   title = temp_title_fin , show=False)
+        plt.savefig(temp_file,bbox_inches='tight')    
+        
         adata_all_cluster.write_csvs(temp_output)
         
         #saving the distances of the knn graph
         c = sp.sparse.csc_matrix.todense(adata_all_cluster.obsp['distances'])      
         np.savetxt(temp_file_distances, c)
-
